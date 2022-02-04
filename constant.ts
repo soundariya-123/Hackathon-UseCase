da94b103c080cbf84da100d1d7a8470a2acf7b39
Sairam Sahu <54425384+sairamsahu@users.noreply.github.com>
2022-02-04 15:08:48 +0530

    Add files via upload

---
+++ constant.ts
@@ -0,0 +1,72 @@
+export const FIND_LOCATIONS= `Location Details With Doctors Details Fetched Successfully`
+export const FIND_LOCATIONS_NOTFOUND= `Location Details With Doctors Details Not Found`
+export const REAPPOINTMENT_SCHEDULE= `Reappointment Schedule Booked successfully`
+export const REAPPOINTMENT_SCHEDULE_FAILED= `Reappointment Schedule Booking Failed`
+export const USERDETAILS_FAILED= `User Details Not Found`
+/**
+ * Constant REGEX
+ */
+ export const REGEX = {
+    PASSWORD_RULE : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+}
+/**
+ * Constant USER_NOT_FOUND
+ */
+export const USER_NOT_FOUND = `User Not found`;
+/**
+ * Constant REGISTER_INTERNALSERVER_FAILED
+ */
+export const REGISTER_INTERNALSERVER_FAILED = `Unable To Register Please Try Later`;
+
+/**
+ * Constant EMAILID_CONFLICT_EXCEPTION
+ */
+export const EMAILID_CONFLICT_EXCEPTION = `User already Exists`;
+
+/**
+ * Constant USER_INVALID_CREDENTIALS
+ */
+export const USER_INVALID_CREDENTIALS = 'Invalid Credentials';
+
+/**
+ * Constant USERS_MESSAGE
+ */
+export const USERS_MESSAGE= 'User Registeration Sucessfully';
+
+/**
+ * Constant USERS_LOGIN_MESSAGE
+ */
+export const USERS_LOGIN_MESSAGE = `User Authenticated Sucessfully`;
+
+/**
+ * Constant SLOT_SUCCESS_MESSAGE
+ */
+export const SLOT_SUCCESS_MESSAGE = `Slot Created Sucessfully`;
+
+/**
+ * Constant SLOT_FAILED_MESSAGE
+ */
+ export const SLOT_FAILED_MESSAGE = `Slot not created`;
+
+/**
+ * Constant SLOT_CANCELLED_MESSAGE
+ */
+export const SLOT_CANCELLED_MESSAGE = `Slot Cancelled Sucessfully`;
+
+/**
+ * Constant RESCHEDULE_SLOT_MESSAGE
+ */
+export const RESCHEDULE_SLOT_MESSAGE = `Reschedule Sucessfully`;
+
+/**
+ * Constant APPOINMENT_BOOKED_MESSAGE
+ */
+export const APPOINMENT_BOOKED_MESSAGE = `Appoinment booked Sucessfully`;
+
+/**
+ * Constant APPOINMENT_DELETED_MESSAGE
+ */
+export const APPOINMENT_DELETED_MESSAGE = `Appoinment deleted Sucessfully`;
+ 
+
